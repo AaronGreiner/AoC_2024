8408b1e4001bfa5c543679dd41b382c65b61a420
Aaron Greiner <aarongreiner@icloud.com>
2024-12-01 18:16:30 +0100

    init day 1

---
+++ AoC_Day_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AoC_Day_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"../../../input.txt");
+
+            int totalDistance = 0;
+            int similarityScore = 0;
+
+            List<int> leftList = new List<int>();
+            List<int> rightList = new List<int>();
+
+            foreach (string s in input)
+            {
+                string[] numbers = s.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                leftList.Add(int.Parse(numbers[0]));
+                rightList.Add(int.Parse(numbers[1]));
+            }
+
+            leftList.Sort();
+            rightList.Sort();
+
+            for (int i = 0; i < leftList.Count; i++)
+            {
+                totalDistance += Math.Abs(leftList[i] - rightList[i]);
+            }
+
+            foreach (int num in leftList)
+            {
+                similarityScore += num * rightList.Count(x => x == num);
+            }
+
+            Console.WriteLine("Total distance between the lists: " + totalDistance);
+            Console.WriteLine("Total similarity score: " + similarityScore);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# AoC_2024
+# Advent of Code 2024
+<https://adventofcode.com/2024>
+***
+###### Output Day 1
+>Total distance between the lists: **2580760**<br>
+>Total similarity score: **25358365** <sup>*Bonus</sup>
+***
